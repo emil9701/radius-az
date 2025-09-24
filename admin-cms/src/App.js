fae95b331903a6fd5bc1b21ae869a14d56289bb8
emil9701 <emil.abdulla@gmail.com>
2025-09-24 12:08:14 +0400

    Add App.js scaffold for CMS Admin Panel

---
+++ admin-cms/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>CMS Admin Panel Scaffold</h1>
+      <p>Welcome to your new CMS admin panel!</p>
+    </div>
+  );
+}
+
+export default App;
